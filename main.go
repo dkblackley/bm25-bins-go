496745efe1b370fa29454bc9e7aff0d0174e0332
dkblackley <dblackle@gmu.edu>
2025-11-24 11:14:33 -0500

    Potential fix

--- main.go
+++ main.go
@@ -29,8 +29,9 @@ import (
 
 const MAX_UINT32 = ^uint32(0)
 
-// const MARCO_SIZE = 8841823
-const MARCO_SIZE = 1105 //Debug size
+const MARCO_SIZE = 8841823
+
+// const MARCO_SIZE = 1105 //Debug size
 const DIM = 192
 const RTT = 50
 
